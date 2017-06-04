c78594cbfbd676074363389eb45fc9f5da0b7115
Brian Be <bebrian458@gmail.com>
2017-06-04 16:29:12 -0700

    Implemented basic checkblock for direct blocks, still need to work on indirect

--- lab3b.py
+++ lab3b.py
@@ -22,7 +22,41 @@ class Groupdesc:
 		self.g_inode_bitmap_num = 0
 		self.g_itable_num = 0
 
+class Block:
+	def __init__(self, blocknum, blocktype, blockstate, inode_num, offset_num):
+		self.blocknum = blocknum
+		self.blocktype = blocktype
+		self.blockstate = blockstate
+		self.inode_num = inode_num
+		self.offset_num = offset_num
 
+class Inode:
+	def __init__(self, inode_num, numlinks):
+		self.inode_num = inode_num
+		self.numlinks = numlinks
+		self.block_pointers = []
+		self.blocks = []
+
+class Dirent:
+	def __init__(self, parent_inode_num, child_inode_num, name):
+		self.parent_inode_num = parent_inode_num
+		self.child_inode_num = child_inode_num
+		self.name = name
+
+class Indirect:
+	def __init__(self, inode_num, level, block_offset):
+		self.inode_num = inode_num
+		self.level = level
+		self.block_offset = block_offset
+
+
+# Globals
+superblock = Superblock()
+groupdesc = Groupdesc()
+bfree_list = []
+ifree_list = []
+inode_list = []
+start_data_blocks = 1	# change later
 
 def readSuperblock(superblock):
 	with open(arg1, 'r') as csvfile:
@@ -30,13 +64,13 @@ def readSuperblock(superblock):
 		for r in reader:
 			if r[0] == "SUPERBLOCK":
 				#print r
-				superblock.num_blocks = r[1]
-				superblock.num_inodes = r[2]
-				superblock.blocksize = r[3]
-				superblock.inodesize = r[4]
-				superblock.blocks_per_group = r[5]
-				superblock.inodes_per_group = r[6]
-				superblock.first_nr_ino = r[7]
+				superblock.num_blocks = int(r[1])
+				superblock.num_inodes = int(r[2])
+				superblock.blocksize = int(r[3])
+				superblock.inodesize = int(r[4])
+				superblock.blocks_per_group = int(r[5])
+				superblock.inodes_per_group = int(r[6])
+				superblock.first_nr_ino = int(r[7])
 
 def readGroupdesc(groupdesc):
 	with open(arg1, 'r') as csvfile:
@@ -44,59 +78,125 @@ def readGroupdesc(groupdesc):
 		for r in reader:
 			if r[0] == "GROUP":
 				#print r
-				groupdesc.group_num = r[1]
-				groupdesc.g_num_blocks = r[2]
-				groupdesc.g_num_inodes = r[3]
-				groupdesc.g_num_free_blocks = r[4]
-				groupdesc.g_num_free_inodes = r[5]
-				groupdesc.g_block_bitmap_num = r[6]
-				groupdesc.g_inode_bitmap_num = r[7]
-				groupdesc.g_itable_num = r[8]
+				groupdesc.group_num = int(r[1])
+				groupdesc.g_num_blocks = int(r[2])
+				groupdesc.g_num_inodes = int(r[3])
+				groupdesc.g_num_free_blocks = int(r[4])
+				groupdesc.g_num_free_inodes = int(r[5])
+				groupdesc.g_block_bitmap_num = int(r[6])
+				groupdesc.g_inode_bitmap_num = int(r[7])
+				groupdesc.g_itable_num = int(r[8])
 
 def readBfree(list):
 	with open(arg1, 'r') as csvfile:
 		reader = csv.reader(csvfile, delimiter=',', quotechar='|')
 		for r in reader:
 			if r[0] == "BFREE":
-				list.append(r[1])
+				list.append(int(r[1]))
 
 def readIfree(list):
 	with open(arg1, 'r') as csvfile:
 		reader = csv.reader(csvfile, delimiter=',', quotechar='|')
 		for r in reader:
 			if r[0] == "IFREE":
-				list.append(r[1])
+				list.append(int(r[1]))
 
+def readInode(list):
+	with open(arg1, 'r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+		for r in reader:
+			if r[0] == "INODE":
+				new_inode = Inode(int(r[1]), int(r[6]))
+				for index in range(12, 27):
+					new_inode.block_pointers.append(int(r[index]))
+					new_block = Block(int(r[index]), "BLOCK", "NONE", int(r[1]), index)
+					new_inode.blocks.append(new_block)
+				list.append(new_inode)
+
+def isValidBlock(block):
+	
+	# Check block number
+	if block.blocknum < 0 or block.blocknum > superblock.num_blocks:
+		block.blockstate = "INVALID"
+		print "INVALID", block.blocktype, block.blocknum, "IN INODE", block.inode_num, "AT OFFSET", block.offset_num
+		return 0
+
+	# Check inode number
+	if block.inode_num < 0 or block.inode_num > superblock.num_inodes or (block.inode_num > 2 and block.inode_num < superblock.first_nr_ino):
+		block.blockstate = "INVALID"
+		print "INVALID", block.blocktype, block.blocknum, "IN INODE", block.inode_num, "AT OFFSET", block.offset_num
+		return 0
+
+	return 1
+
+def checkReserved(block):
+	if block.blocknum >= 0 and block.blocknum < start_data_blocks:
+		block.blockstate = "RESERVED"
+		print "RESERVED", block.blocktype, block.blocknum, "IN INODE", block.inode_num, "AT OFFSET", block.offset_num
+		return 1
+	return 0
+
+def checkFree(block):
+	if block.blocknum in bfree_list:
+		block.blockstate = "BFREE"
+		print "ALLOCATED BLOCK", block.blocknum, "ON FREELIST"
+		return 1
+	return 0
+
+def updateState(block):
+	if block.blockstate == "NONE":
+		block.blockstate = "IN_USE"
+		# print "IN_USE", block.blocktype, block.blocknum, "IN INODE", block.inode_num, "AT OFFSET", block.offset_num
+	elif block.blockstate == "IN_USE" or block.blockstate == "DUPLICATE":
+		block.blockstate = "DUPLICATE"
+		print "DUPLICATE", block.blocktype, block.blocknum, "IN INODE", block.inode_num, "AT OFFSET", block.offset_num
+
+def checkBlock(block):
+	
+	'''
+	States:
+	NONE 	- default
+	INVALID - blocknum < 0 or blocknum > numblocks
+	VALID:
+		RESERVED 	- [1:start of legal data blocks]
+		BFREE 		- on the bfree list
+		NONE 	-> IN_USE
+		IN_USE 	-> DUPLICATE
+		DUPLICATE 	- block is referenced multiple times
+	UNREFERENCED 	- if after all assignments and state is still none
+	'''
+
+	# Assign new states to the block
+	if isValidBlock(block) == 1:
+		if checkReserved(block) != 1:
+		 	if checkFree(block) != 1:
+				updateState(block)
+
+	# At this point, referenced blocks should be assigned a state other than none
+	if block.blockstate == "NONE":
+		block.blockstate = "UNREFERENCED"
+		print "UNREFERENCED BLOCK", block.blocknum
 
 
 
 def main():
 	print "The input file name is:", arg1
-	superblock = Superblock()
-	groupdesc = Groupdesc()
-	bfree_list = []
-	ifree_list = []
 	readSuperblock(superblock)
-	# print superblock.num_blocks
-	# print superblock.num_inodes
-	# print superblock.blocksize
-	# print superblock.inodesize
-	# print superblock.blocks_per_group
-	# print superblock.inodes_per_group
-	# print superblock.first_nr_ino
+	print superblock.blocksize
 	readGroupdesc(groupdesc)
-	# print groupdesc.group_num
-	# print groupdesc.g_num_blocks
-	# print groupdesc.g_num_inodes
-	# print groupdesc.g_num_free_blocks
-	# print groupdesc.g_num_free_inodes
-	# print groupdesc.g_block_bitmap_num
-	# print groupdesc.g_inode_bitmap_num
-	# print groupdesc.g_itable_num
 	readBfree(bfree_list)
 	readIfree(ifree_list)
-	# for elem in ifree_list:
-	# 	print elem
+	blocks_occupied_by_itable = (superblock.inodesize*superblock.inodes_per_group)/superblock.blocksize
+	start_data_blocks =  blocks_occupied_by_itable + groupdesc.g_itable_num
+	readInode(inode_list)
+	for i in inode_list:
+		# print i.block_pointers
+		# print "This is the ith index of inode list", i
+		# print i.block_pointers[12]
+		# print i.block_pointers[13]
+		# print i.block_pointers[14]
+		for index in range (0,11):
+			checkBlock(i.blocks[index])
 
 
 
